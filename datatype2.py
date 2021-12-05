7d205a9b4bd9c66e935063fab74974b8f7b3751b
dipeshpatidarjeeran <dipeshjeeran@gmail.com>
2021-12-05 18:36:30 +0530

    listandtuple

---
+++ datatype2.py
@@ -0,0 +1,24 @@
+x = 4  #int
+y = 6  #int
+
+print(x + y)
+ 
+a = 10  #int
+b = 35  #int
+c = 2j  #complex
+
+print(a+b)
+
+print(type(2j))
+# python list []
+#in python list allow dublicate elements
+friends = ['ajay','sajid','deepak','ritik','ritik']
+print(friends)
+print(friends[3])
+msg = ['python',5,'ajay',5.4]
+print(msg)
+print('I am learning '+msg[0]+' from ' +friends[3])
+# data type 
+#Tuple
+student = ('dipesh','sajid','deepak')
+print(student[2])
